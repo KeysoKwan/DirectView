a1e9afad66abad579cc9f844f8c2a25c9e7c2dc2
daixian <amano_tooko@qq.com>
2019-04-17 21:16:20 +0800

    重新整理了一下工程

--- U3D/Assets/Test/FView.cs
+++ U3D/Assets/Test/FView.cs
@@ -16,36 +16,62 @@ public class FView : MonoBehaviour
 {
 
     [DllImport("FView")]
-    private static extern void ShowInExe(System.IntPtr hWnd, System.IntPtr textureHandle, int w, int h);
+    private static extern void StartView(System.IntPtr hWnd, System.IntPtr textureHandle, int w, int h);
 
     //寻找当前目标窗口的进程
     [DllImport("user32.dll")]
     private static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
+
     //根据窗口句柄获取pid
     [DllImport("User32.dll")]
     private static extern int GetWindowThreadProcessId(IntPtr hwnd, out int ID);
 
-
+    /// <summary>
+    /// 相机采图
+    /// </summary>
     private WebCamTexture _camTex;
 
+    /// <summary>
+    /// 相机背景图
+    /// </summary>
     public RawImage riC920;
 
+    /// <summary>
+    /// 渲染结果
+    /// </summary>
     public RenderTexture rt;
 
+    /// <summary>
+    /// 渲染纹理指针
+    /// </summary>
     private IntPtr rtPtr = IntPtr.Zero;
-    IntPtr VoxenShareWinInptr = IntPtr.Zero;
+
+    /// <summary>
+    /// 显示窗口句柄
+    /// </summary>
+    IntPtr _hViewClient = IntPtr.Zero;
 
     /// <summary>
     /// 一个罗技摄像头姿态的记录
     /// </summary>
     FViewRT fViewRT;
 
+    /// <summary>
+    /// 显示窗口进程
+    /// </summary>
+    Process viewProcess;
+
     private void Awake()
     {
         //程序一运行就绑定一下
         JsonTypeBind.Bind();
-
         string fViewJsonPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "FViewTool", "FView.json");
+
+        if (!File.Exists(fViewJsonPath))
+        {
+            UnityEngine.Debug.LogError("FView.OpenFViewWindows():先使用工具软件进行罗技摄像头的标定！");
+        }
+
         //读取json的配置
         string str = File.ReadAllText(fViewJsonPath);
         fViewRT = JsonMapper.ToObject<FViewRT>(str);
@@ -63,8 +89,6 @@ public class FView : MonoBehaviour
         await Task.Delay(3000);
 
         OpenFViewWindows();
-
-
     }
 
     // Update is called once per frame
@@ -76,16 +100,12 @@ public class FView : MonoBehaviour
         }
 
         IntPtr curRtPtr = rt.GetNativeTexturePtr();
-        if (curRtPtr != rtPtr)
+        if (curRtPtr != rtPtr && _hViewClient != IntPtr.Zero)
         {
             rtPtr = curRtPtr;
-            UnityEngine.Debug.Log("FView.Update():当前纹理指针赋值!" + rtPtr.ToInt64());
-            //调用c++dll
-            ShowInExe(VoxenShareWinInptr, rtPtr, 1920, 1080);
+            StartView(_hViewClient, rtPtr, 1920, 1080);
         }
     }
-
-    Process viewProcess;
 
     private void OpenFViewWindows()
     {
@@ -96,21 +116,20 @@ public class FView : MonoBehaviour
         string _path = Path.Combine(Application.streamingAssetsPath, "ClientWin.exe");
         ProcessStartInfo startInfo = new ProcessStartInfo();
         startInfo.FileName = _path;
-        startInfo.Arguments = "OpenInUnity";
         viewProcess = new Process();
         viewProcess.StartInfo = startInfo;
         viewProcess.Start();
 
 
-        VoxenShareWinInptr = IntPtr.Zero;
+        _hViewClient = IntPtr.Zero;
         while (true)
         {
-            VoxenShareWinInptr = FindWindow(null, "ViewClient");
-            if (VoxenShareWinInptr != IntPtr.Zero)
+            _hViewClient = FindWindow(null, "ViewClient");
+            if (_hViewClient != IntPtr.Zero)
             {
                 UnityEngine.Debug.Log("FView.OpenFViewWindows():找到了窗口句柄！");
                 int pid = 0;
-                GetWindowThreadProcessId(VoxenShareWinInptr, out pid);
+                GetWindowThreadProcessId(_hViewClient, out pid);
                 if (pid == viewProcess.Id)
                 {
                     break;
@@ -122,15 +141,6 @@ public class FView : MonoBehaviour
 
     }
 
-
-    private void OnGUI()
-    {
-        if (GUILayout.Button("open"))
-        {
-            OpenFViewWindows();
-        }
-    }
-
     private void OnApplicationQuit()
     {
         if (viewProcess != null)
@@ -140,6 +150,10 @@ public class FView : MonoBehaviour
 
     }
 
+    /// <summary>
+    /// 打开罗技HD Pro Webcam C920的相机
+    /// </summary>
+    /// <returns></returns>
     IEnumerator InitCamera()
     {
 
@@ -148,7 +162,6 @@ public class FView : MonoBehaviour
         if (Application.HasUserAuthorization(UserAuthorization.WebCam))
         {
             var devices = WebCamTexture.devices;
-            //UnityEngine.Debug.Log("devices.Length " + devices.Length);
             if (devices.Length > 4)
             {
                 string _deviceName = "";
@@ -160,8 +173,6 @@ public class FView : MonoBehaviour
                         break;
                     }
                 }
-
-                //var _deviceName = "HD Pro Webcam C920";//devices[0].name;
                 if (string.IsNullOrEmpty(_deviceName))
                 {
                     UnityEngine.Debug.LogError("ScreenControlObj.InitCamera():相机启动失败，没有外接相机");
@@ -182,10 +193,6 @@ public class FView : MonoBehaviour
                     //_tex.anisoLevel = 2;
                     UnityEngine.Debug.Log("ScreenControlObj.InitCamera():相机启动");
                 }
-
-
-
-
             }
         }
     }
--- U3DShow/U3DShow/Show.h
+++ U3DShow/U3DShow/Show.h
@@ -2,7 +2,6 @@
 
 #define _VSAPI_ extern "C" __declspec(dllexport)
 
+_VSAPI_ int StartView(HWND hWnd, void* textureHandle, int w, int h);
 
-_VSAPI_ void StopShow();
-
-_VSAPI_ int ShowInExe(HWND hWnd, void* textureHandle, int w, int h);
+_VSAPI_ void StopView();
