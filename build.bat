ce3a105d89949a73d3cd1dbe38d723dfd369cf54
daixian <amano_tooko@qq.com>
2019-05-31 18:46:18 +0800

    增加json库

--- build.bat
+++ build.bat
@@ -1,5 +1,5 @@
 python -V
-python ./tools/DownloadLib.py -d "C:/dxlib/download" -l "C:/dxlib/lib" "concurrentqueue" "dlog" "gtest" "eigen"
+python ./tools/DownloadLib.py -d "C:/dxlib/download" -l "C:/dxlib/lib" "concurrentqueue" "dlog" "gtest" "eigen" "rapidjson"
 REM mkdir build
 REM cd build
 REM mkdir Win64
